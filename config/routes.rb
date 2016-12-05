96516681eddfd047d9d89311ad3102b4c8c6262b
Tomi <tomi.tapio.laaksonen@gmail.com>
2016-12-05 05:53:25 +0000

    UI for Stocks

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
   
 
   # Example of regular route:
