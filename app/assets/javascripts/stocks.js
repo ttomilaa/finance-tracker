2dc56755f8cc6621ea8753ddc88c771e17a85043
Tomi <tomi.tapio.laaksonen@gmail.com>
2016-12-08 16:28:43 +0000

    bug fixes

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -10,8 +10,8 @@ init_stock_lookup = function() {
   });
   
   $('#stock-lookup-form').on('ajax:success', function(event, data, status){
-  $('#stock-lookup').replaceWith(data);
-  init_stock_lookup();
+    $('#stock-lookup').replaceWith(data);
+    init_stock_lookup();
   });
   
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
